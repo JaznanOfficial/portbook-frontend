073b20954f003690d2922d3a0403ab5a910d737d
JaznanOfficial <jaznanofficial@gmail.com>
2023-05-18 08:03:18 +0600

    like handler done

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
     reducerPath: "api",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/v1/" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://rojnamcha.onrender.com/api/v1/" }),
     tagTypes: [],
     endpoints: (builder) => ({}),
 });
